ad4cca5ce326b18499597294a0eeb126b5157a33
Aliaksandr Kandratsiuk <a.kandratsiuk@godeltech.com>
2021-03-17 21:31:12 +0200

    Change readme (#5)

--- src/CodeReview.FileConverter/Options/OptionsBase.cs
+++ src/CodeReview.FileConverter/Options/OptionsBase.cs
@@ -7,7 +7,7 @@ namespace GodelTech.CodeReview.FileConverter.Options
         [Option('f', "folder", Required = true, HelpText = "Path to folder or file to process")]
         public string Path { get; set; }
         
-        [Option('m', "mask", Default = "*", Required = false, HelpText = "Search mast used to look for files within folder")]
+        [Option('m', "mask", Default = "*", Required = false, HelpText = "Search mask used to look for files within folder")]
         public string SearchMask { get; set; }
 
         [Option('r', "recurse", Default = true, Required = false, HelpText = "Specifies if recurse search must be used for for files in folder")]
