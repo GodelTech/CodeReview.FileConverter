cb05dd56d54e13509a9a52a1a5561b000af630bf
Pavel Lapatsin <pavel.lapatsin@sorted.com>
2022-05-26 09:07:46 +0100

    Update dependency check file convertor. Add healthy dependencies into results

--- src/CodeReview.FileConverter/Services/DependencyCheckFileConverter.cs
+++ src/CodeReview.FileConverter/Services/DependencyCheckFileConverter.cs
@@ -23,11 +23,11 @@ namespace GodelTech.CodeReview.FileConverter.Services
         {
             var issues = new List<Issue>();
 
-            var vulnerableDependencies = await GetVulnerableDependencies(filePath);
+            var dependencies = await GetVulnerableDependencies(filePath);
 
-            foreach (var vulnerableDependency in vulnerableDependencies)
+            foreach (var dependency in dependencies)
             {
-                issues.AddRange(GetIssues(vulnerableDependency));
+                issues.AddRange(GetIssues(dependency));
             }
 
             return issues;
@@ -48,53 +48,94 @@ namespace GodelTech.CodeReview.FileConverter.Services
             var dependencyCheckResult = await JsonSerializer.DeserializeAsync<DependencyCheckResult>(fileStream, options);
 
             return dependencyCheckResult
-                .Dependencies?
-                .Where(x => x.Vulnerabilities != null && x.Vulnerabilities.Any()) 
-                   ?? Enumerable.Empty<Dependency>();
+                .Dependencies ?? Enumerable.Empty<Dependency>();
         }
 
-        private IEnumerable<Issue> GetIssues(Dependency vulnerableDependency)
+        private IEnumerable<Issue> GetIssues(Dependency dependency)
         {
-            foreach (var vulnerability in vulnerableDependency.Vulnerabilities)
+            if (IsValnurableDependency(dependency))
+            {
+                foreach (var vulnerability in dependency.Vulnerabilities)
+                {
+                    yield return new Issue
+                    {
+                        Id = _idGenerator.GetNext(),
+                        Title = "Component with known vulnerabilities discovered",
+                        Description = GetDescriptionForValnurableDependency(dependency.FileName, vulnerability.Description),
+                        Category = "External valnurable dependency",
+                        DetailsUrl = vulnerability.DetailsUrl,
+                        Level = ConvertLevel(vulnerability.Severity),
+                        Message = GetMessage(vulnerability.Cvssv3, vulnerability.Cvssv2),
+                        RuleId = vulnerability.Name,
+                        Locations = new[]
+                        {
+                            GetIssueLocations(dependency)
+                        },
+                        Properties = AddProperties(vulnerability.Cvssv2, vulnerability.Cvssv3)
+                    };
+                }
+            }
+            else
             {
                 yield return new Issue
                 {
                     Id = _idGenerator.GetNext(),
-                    Title = "Component with known vulnerabilities discovered",
-                    Description = GetDescription(vulnerableDependency.FileName, vulnerability.Description),
-                    Category = "Vulnerable Dependency",
-                    DetailsUrl = vulnerability.DetailsUrl,
-                    Level = ConvertLevel(vulnerability.Severity),
-                    Message = GetMessage(vulnerability.Cvssv3),
-                    RuleId = vulnerability.Name,
+                    RuleId = "Component",
+                    Message = "Component does not contain known vulnarability",
+                    Title = "Component without discovered vulnerabilities",
+                    Description = GetDescriptionForDependency(dependency.FileName),
+                    Category = "External dependency",
+                    Level = IssueLevel.None,
                     Locations = new[]
                     {
-                        GetIssueLocations(vulnerableDependency)
-                    },
-                    Properties = new Dictionary<string, string>
-                    {
-                        {
-                            "BaseScore",
-                            vulnerability.Cvssv3.BaseScore.ToString()
-                        }
+                        GetIssueLocations(dependency)
                     }
                 };
             }
         }
 
-        private string GetDescription(string fileName, string description)
+        private Dictionary<string, string> AddProperties(Cvssv2 cvssv2, Cvssv3 cvssv3)
+        {
+            var properties = new Dictionary<string, string>();
+
+            var score = cvssv3?.BaseScore ?? cvssv2?.Score;
+
+            if(score != null)
+            {
+                properties.Add("score", score.ToString());
+            }
+
+            return properties;
+        }
+
+        private string GetDescriptionForDependency(string fileName)
+        {
+            return $"Project uses dependency {fileName}.";
+        }
+
+        private bool IsValnurableDependency(Dependency dependency)
+        {
+            return dependency.Vulnerabilities != null && dependency.Vulnerabilities.Any();
+        }
+
+        private string GetDescriptionForValnurableDependency(string fileName, string description)
         {
             return $"Project uses vulnerable dependency {fileName}. {description}";
         }
 
-        private string GetMessage(Cvssv3 cvssv3)
+        private string GetMessage(Cvssv3 cvssv3, Cvssv2 cvssv2)
         {
             if (cvssv3 == null)
             {
-                return string.Empty;
+                return cvssv2 != null ? BuildMessageForCvssv2(cvssv2) : string.Empty;
             }
 
-            return $"Common Vulnerability Scoring System rates: BaseScore {cvssv3.BaseScore}," +
+            return BuildMessageForCvssv3(cvssv3);
+        }
+
+        private string BuildMessageForCvssv3(Cvssv3 cvssv3)
+        {
+            return $"Common Vulnerability Scoring System of 3 version. Defenition: BaseScore {cvssv3.BaseScore}," +
                    $" AttackVector {cvssv3.AttackVector}, AttackComplexity {cvssv3.AttackComplexity}," +
                    $" PrivilegesRequired {cvssv3.PrivilegesRequired}, UseInteraction {cvssv3.UserInteraction}," +
                    $" Scope {cvssv3.Scope}, ConfidentialityImpact {cvssv3.ConfidentialityImpact}," +
@@ -102,11 +143,21 @@ namespace GodelTech.CodeReview.FileConverter.Services
                    $" BaseSeverity {cvssv3.BaseSeverity}";
         }
 
-        private IssueLocation GetIssueLocations(Dependency vulnerableDependency)
+        private string BuildMessageForCvssv2(Cvssv2 cvssv2)
+        {
+            return $"Common Vulnerability Scoring System of 2 version. Defenition: Score {cvssv2.Score}," +
+                    $" AccessVector {cvssv2.AccessVector}, AccessComplexity {cvssv2.AccessComplexity}," +
+                    $" Authenticationr {cvssv2.Authenticationr}, ConfidentialImpact {cvssv2.ConfidentialImpact}," +
+                    $" IntegrityImpact {cvssv2.IntegrityImpact}, AvailabilityImpact {cvssv2.AvailabilityImpact}," +
+                    $" Severity {cvssv2.Severity}, Version {cvssv2.Version}," +
+                    $" ExploitabilityScore {cvssv2.ExploitabilityScore}, ImpactScore {cvssv2.ImpactScore}";
+        }
+
+        private IssueLocation GetIssueLocations(Dependency dependency)
         {
             return new IssueLocation
             {
-                FilePath = vulnerableDependency.FilePath,
+                FilePath = $"{dependency.FilePath}/{dependency.FileName}",
             };
         }
 
